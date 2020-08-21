1851870298d1ffe43a6624bceef93393acaf4324
anacsv <anapaula.carneiro@olist.com>
2020-08-21 11:56:34 -0300

    Add js archives

---
+++ templates/js/seller_order.js
@@ -0,0 +1,110 @@
+var seller_orders_api = 'http://127.0.0.1:5000/api/seller-orders/'
+
+// ---------------------------------------- Load data Json File
+function load_data_json(){
+    $.ajax({
+        url : seller_orders_api
+        ,dataType : 'json'
+        ,type : 'get'
+        ,success: (data)=>{
+            load_data(data);
+        },
+        error:(e)=>{
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+// ---------------------------------------- END Load data Json File
+function update(data, id) {
+    $.ajax({
+        type : 'PUT',
+        url: seller_orders_api+id,
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: () => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+
+function toJson(data) {
+    let obj = {};
+    jQuery.map(data, function(n, i) {
+        obj[n.name] = n.value;
+    });
+    return obj
+}
+
+// ---------------------------------------- Load Json 
+function load_data(data){ 
+    data.forEach(e => {
+        data += `<tr>
+            <td>${e['code']}</td>
+            <td>${e['order_id']}</td>"
+            <td>${e['seller_name']}</td>"
+            <td>${e['status']}</td>
+            <td>${e['cancelation_status']}</td>"
+            <td data-id="${e['id']}" >
+                <a class='btn-edit' href='seller-orders/form.html?id=${e['id']}'>Editar</a> |
+                <a class='btn-delete' href='#'>Deletar</a>
+            </td>
+        </tr>`;
+    });
+    
+    $('table tbody').html(data).promise().done(()=>{
+        $('.btn-edit').click( (event)=>btnClick(event) );
+        $('.btn-delete').click( (event)=>btnDelete(event) );
+    });
+}
+// ---------------------------------------- END Load Json 
+
+function delete_data(id){
+    $.ajax({
+        url : seller_orders_api + id,
+        type: 'DELETE',
+        success: (data) => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    })
+}
+
+
+// ---------------------------------------- Find id in Json File and Load html
+function findById(id){
+    $.ajax({
+        url : seller_orders_api+id
+        ,dataType : 'json'
+        ,type : 'get'
+        ,success: (data)=>{
+            $("[name='code']").val(data.code);
+            $("[name='order_id']").val(data.order_id);
+            $("[name='seller_name']").val(data.seller_name);
+            $("[name='status']").val(data.status);
+            $("[name='cancelation_status']").val(data.cancelation_status);
+        }
+    });
+}
+// ---------------------------------------- END Find id in Json File and Load html
+
+
+function save(data) {
+    $.ajax({
+        type : 'POST',
+        url: seller_orders_api,
+        contentType: 'application/json',
+        data: data,
+        success: () => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+$(document).ready(()=>load_data_json());
